16007e39e60d3bdb642762adabdbeecd636f171a
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-06-27 15:25:16 -0400

    count 1-10

---
+++ forLoops.js
@@ -0,0 +1,31 @@
+
+// prime numbers 
+
+// for(let i = 1; i <= 20; i++){
+//     if (i % 2 == 0){
+//         console.log(i + "even")
+//     }else if (i % 3 == 0){
+//         console.log("odd" +i);
+//     }else{
+//         console.log('prime' +'' + i)
+//     }
+// }
+
+//even numbers 
+// for(counter=0; counter <=100; counter++){
+//     if(counter % 3==0){
+//         console.log(counter);
+//     }
+// }
+
+//triangle Loop
+
+// var triangle = '#';
+// for (var i =0; i < 11; i++){
+//     console.log(triangle += '#');
+// }
+
+//counter 1-10
+for (let counter = 0; counter < 10; counter ++){
+    console.log(counter);
+}
