1075d9a7fdcb3f4154ad3fd333495c730d09bf9b
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-06-27 18:09:19 -0400

    operators

---
+++ operators.js
@@ -0,0 +1,22 @@
+//comparison
+
+let x =1
+ //relational
+
+ console.log(x >0);
+ console.log(x >=1);
+ console.log(x <1);
+ console.log(x <=1);
+
+ //equality
+ console.log(x===1);
+ console.log(x !==1);
+
+
+ // Ternary Operator 
+ 
+ let points = 110;
+ let type = points > 100 ? 'gold' : 'silver';
+ console.log(type)
+
+
