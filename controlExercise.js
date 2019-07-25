f31b25b743ee2597e10747c80bb8ba8df8f2d21c
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-07-25 18:06:05 -0400

    added more

--- controlExercise.js
+++ controlExercise.js
@@ -19,7 +19,20 @@ return(width > height);
 //divisible by 3 n 5 fizzBuzz
 // not divisible by 3 or 5  => input
 
-
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i + " FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log(i + " Fizz");
+    }
+    else if (i % 5 === 0) {
+        console.log(i + " Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
 
 // 5- Even and Odd Numbers
 
@@ -30,3 +43,33 @@ function showNumbers(limit){
         console.log(i, message);
     }
 }
+
+// Average grade
+
+var students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+var Avgmarks = 0;
+
+for (var i = 0; i < students.length; i++) {
+    Avgmarks += students[i][1];
+    var avg = (Avgmarks / students.length);
+}
+
+console.log("Average grade: " + (Avgmarks) / students.length);
+
+if (avg < 60) {
+    console.log("Grade : F");
+}
+else if (avg < 70) {
+    console.log("Grade : D");
+}
+else if (avg < 80) {
+    console.log("Grade : C");
+} else if (avg < 90) {
+    console.log("Grade : B");
+} else if (avg < 100) {
+    console.log("Grade : A");
+}
+
+// triangle 
+for (let i = "*"; i.length < 6; i += "*") console.log(i);
