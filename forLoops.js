d3b6ec2aaaf5af1f4c97bc2ee2ea39c70445ec09
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-07-25 19:02:02 -0400

    added more

--- forLoops.js
+++ forLoops.js
@@ -72,6 +72,7 @@ for (var x = 0; x <= 15; x++) {
     if (x === 0) {
         console.log(x + " is even");
     }
+
     else if (x % 2 === 0) {
         console.log(x + " is even");
     }
@@ -79,3 +80,46 @@ for (var x = 0; x <= 15; x++) {
         console.log(x + " is odd");
     }
 }
+
+// Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference.
+
+function difference(n) {
+    if (n <= 13)
+        return 13 - n;
+    else
+        return (n - 13) * 2;
+}
+console.log(difference(32))
+console.log(difference(11))
+
+// ES6 Version
+function difference(n) {
+    if (n <= 13)
+        return 13 - n;
+    else
+        return (n - 13) * 2;
+}
+console.log(difference(32))
+console.log(difference(11))
+        // OR
+function newDif(n) {
+    return (n <= 13) ? 13 - n : (n - 13) * 2;
+}
+
+console.log(newDif(33))
+console.log(newDif(9))
+
+// Write a JavaScript program check if a given positive number is a multiple of 3 or a multiple of 7.
+function test37(x) {
+    if (x % 3 == 0 || x % 7 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(test37(12));
+console.log(test37(14));
+console.log(test37(10));
+console.log(test37(11));
