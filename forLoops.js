160a497e9aaf230e4accc5464b61653a67787ed2
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-07-25 18:37:33 -0400

    added more

--- forLoops.js
+++ forLoops.js
@@ -10,13 +10,36 @@
 //         console.log('prime' +'' + i)
 //     }
 // }
+                    // or
+function isPrime(num) {
+    let test = 2;
+    while (test < num) {
+        if (num % (test) == 0)
+            return false;
+        test++;
+    }
+    return true;
 
+}
 //even numbers 
 // for(counter=0; counter <=100; counter++){
 //     if(counter % 3==0){
 //         console.log(counter);
 //     }
 // }
+
+            // OR
+    for (var x = 0; x <= 15; x++) {
+        if (x === 0) {
+            console.log(x + " is even");
+        }
+        else if (x % 2 === 0) {
+            console.log(x + " is even");
+        }
+        else {
+            console.log(x + " is odd");
+        }
+    }
 
 //triangle Loop
 
@@ -26,6 +49,33 @@
 // }
 
 //counter 1-10
-for (let counter = 0; counter < 10; counter ++){
-    console.log(counter);
+// for (let counter = 0; counter < 10; counter ++){
+//     console.log(counter);
+// }
+
+// for(var i=1; i<=7; i++){
+//     for(var j=1; j<=1; j++){
+//         console.log(j)
+//     }
+// }
+
+for(let counter=0; counter<10; counter++){
+    console.log("hello");
+    if(counter ===1){
+        
+        console.log("this is one");
+    }
+}
+
+// Fizz BUZZ
+for (var x = 0; x <= 15; x++) {
+    if (x === 0) {
+        console.log(x + " is even");
+    }
+    else if (x % 2 === 0) {
+        console.log(x + " is even");
+    }
+    else {
+        console.log(x + " is odd");
+    }
 }
